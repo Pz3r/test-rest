0881f550c1c950818e57e56fd436645090def5ca
Samuel Pozaicer <sam.pz3r@gmail.com>
2016-09-18 17:35:55 -0500

    Added support for ejs templates, and an index page

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+  var apis = {
+    apis: [
+      {path: '/tmnt', description: 'Teenage Mutant Ninja Turtles'}
+    ],
+  };
+
+  res.render('index', apis);
+});
 
 app.get('/tmnt', function(req, res) {
   res.json(
